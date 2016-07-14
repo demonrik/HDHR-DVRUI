b740b017782be852db1ff254b9d3dfab931788a9
Rik Dunphy <rik.dunphy@gmail.com>
2016-07-13 20:09:24 -0700

    clean up vars example

--- app/vars.php
+++ app/vars.php
@@ -1,9 +1,11 @@
 <?php
 final class DVRUI_Vars 
 {
-	const DVRUI_version	="0.1.2";
+	const DVRUI_version	="0.1.3";
 	const DVRUI_name	="HDHomeRun DVR UI";
 	const DVRUI_git		="https://github.com/demonrik/HDHR-DVRUI.git";
-	const DVRUI_TZ		= 'America/Phoenix';
+	
+	/* Set TZ to something valid from http://php.net/manual/en/timezones.php */
+	const DVRUI_TZ		= '';
 }
 ?>
