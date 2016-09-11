35ffcd0059fc88f07c2040b6a052e2705a6a918b
Alex Van de Putte <alex@vandeputte.us>
2016-09-10 21:04:57 -0400

    Update dvrui_common.php

--- app/includes/dvrui_common.php
+++ app/includes/dvrui_common.php
@@ -4,7 +4,7 @@
 			$ch = curl_init();
 			curl_setopt($ch, CURLOPT_URL, $url);
 			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt($ch, CURLOPT_TIMEOUT, 2);
+			curl_setopt($ch, CURLOPT_TIMEOUT, 10);
 			$json = curl_exec($ch);
 			curl_close($ch);
 		} else { 
