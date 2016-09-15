7c5aa6d5070c945e1b095fafc7e79761bdd90c43
Alex Van de Putte <alex@vandeputte.us>
2016-09-15 19:17:28 -0400

    Add files via upload

--- app/includes/dvrui_search.php
+++ app/includes/dvrui_search.php
@@ -37,8 +37,9 @@ class DVRUI_Search {
 
 		// when a series has more than one rule,  then the search result repeats
 		//     seems like a SD bug,  this is the work around
-		$search_info = array_values(array_unique($search_info,SORT_REGULAR));
-;
+		if(count($search_info) > 0){
+			$search_info = array_values(array_unique($search_info,SORT_REGULAR));
+		};
 		for ($i = 0; $i < count($search_info); $i++) {
 			$seriesID = $search_info[$i][$this->search_SeriesID];
 			$image = "";
