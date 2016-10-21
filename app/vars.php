6e93c84fc093c8f3feae49409b5a0eec93630138
Alex Van de Putte <alex@vndptt.com>
2016-10-21 16:01:07 -0400

    added caching capabilities

--- app/vars.php
+++ app/vars.php
@@ -1,11 +1,16 @@
 <?php
 final class DVRUI_Vars 
 {
-	const DVRUI_version	="0.5.0";
+	const DVRUI_version	="0.5.1";
 	const DVRUI_name	="HDHomeRun DVR UI";
 	const DVRUI_git		="https://github.com/demonrik/HDHR-DVRUI.git";
 	
 	/* Set TZ to something valid from http://php.net/manual/en/timezones.php */
 	const DVRUI_TZ		= '';
+
+	/* json cached in seconds */
+	const DVRUI_discover_cache = 120;
+	const DVRUI_upcoming_cache = 3600;
+
 }
 ?>
