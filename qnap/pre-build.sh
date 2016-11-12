b295edb441fa671938c12a5601bb5bc4381ab98c
Rik Dunphy <rik.dunphy@gmail.com>
2016-11-12 11:35:33 -0700

    fixed for discoverURL, addced prebuild for qnap

---
+++ qnap/pre-build.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Simple script to setup the qnap shared directory with the app
+
+APP_DIR='../app'
+SHARED_DIR='./shared'
+CMD_CP='cp -R'
+CMD_RM='rm -r'
+
+case "$1" in
+	prepare)
+		# Copy the app folder
+		$CMD_CP $APP_DIR $SHARED_DIR
+	;;
+
+	clean)
+		# Remove the copied app folder
+		$CMD_RM $APP_DIR $SHARED_DIR/app
+	;;
+
+	*)
+	echo "Usage: $0 {prepare|clean}"
+	exit 1
+esac  
