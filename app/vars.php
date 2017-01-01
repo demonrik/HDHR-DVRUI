bd6d50ba6a76b8613b105401fa14b12d01555ff7
Alex Van de Putte <alex@vndptt.com>
2017-01-01 18:34:22 -0500

    updated to 0.5.10

--- app/vars.php
+++ app/vars.php
@@ -1,7 +1,7 @@
 <?php
 final class DVRUI_Vars 
 {
-	const DVRUI_version	="0.5.9";
+	const DVRUI_version	="0.5.10";
 	const DVRUI_name	="HDHomeRun DVR UI";
 	const DVRUI_git		="https://github.com/demonrik/HDHR-DVRUI.git";
 	
