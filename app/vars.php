e1400345930176583ceb847bb883bbd80ae6c289
Alex Van de Putte <alex@vndptt.com>
2017-07-29 23:55:42 -0400

    New api URL from SD

--- app/vars.php
+++ app/vars.php
@@ -1,10 +1,10 @@
 <?php
 final class DVRUI_Vars 
 {
-	const DVRUI_version	="0.5.12";
+	const DVRUI_version	="0.5.13";
 	const DVRUI_name	="HDHomeRun DVR UI";
 	const DVRUI_git		="https://github.com/demonrik/HDHR-DVRUI.git";
-	
+	const DVRUI_apiurl      ="http://api.hdhomerun.com/";	
 	/* Set TZ to something valid from http://php.net/manual/en/timezones.php */
 	const DVRUI_TZ		= '';
 
