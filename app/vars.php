e30a423f1e1b1d3bf946e99dcb546f4e7205a6f4
Alex Van de Putte <alex@vndptt.com>
2017-07-30 10:18:34 -0400

    bump version number

--- app/vars.php
+++ app/vars.php
@@ -1,7 +1,7 @@
 <?php
 final class DVRUI_Vars 
 {
-	const DVRUI_version	="0.5.13";
+	const DVRUI_version	="0.5.14";
 	const DVRUI_name	="HDHomeRun DVR UI";
 	const DVRUI_git		="https://github.com/demonrik/HDHR-DVRUI.git";
 	const DVRUI_apiurl      ="http://api.hdhomerun.com/";	
