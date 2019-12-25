137aee42fabd4880574c01c53b21e667a916c8c4
Rik Dunphy <rik.dunphy@gmail.com>
2019-12-25 15:38:52 -0700

    fixed to add user agent on every HTTP request

--- app/vars.php
+++ app/vars.php
@@ -1,7 +1,7 @@
 <?php
 final class DVRUI_Vars 
 {
-	const DVRUI_version	="0.8.1";
+	const DVRUI_version	="0.8.2";
 	const DVRUI_name    ="HDHomeRun DVR UI";
 	const DVRUI_git     ="https://github.com/demonrik/HDHR-DVRUI.git";
 	const DVRUI_apiurl  ="http://api.hdhomerun.com/";	
@@ -13,6 +13,6 @@ final class DVRUI_Vars
 	const DVRUI_discover_cache = 360;
 	const DVRUI_upcoming_cache = 3600;
 	
-	const DVRUI_DEBUG = true;
+	const DVRUI_DEBUG = false;
 }
 ?>
